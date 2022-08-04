0f62115eb9cf5ad125737030bde3019b221abdaa
Robert <18058627+RobH0@users.noreply.github.com>
2022-08-04 20:12:17 +0100

    Added functionality that filters CVEs by vendor.

--- track-cves.py
+++ track-cves.py
@@ -2,9 +2,11 @@
 
 # To implement:
 # 1. Download most recent cve data from nvd. Done
-# 2. Add parameters that ask for the user to specify relative time period cves should be displayed for.
-# 3. From that time period only show CVEs relating to specific vendors.
-# 4. Display impact info for cve.
+# 2. Output only CVEs relating to specific vendors. Done
+# 3. Display impact info for cve.
+# 4. Sort CVEs by impact.
+# 5. Add parameters that ask for the user to specify relative time period cves should be displayed for.
+# 6. Use argparse to allow a user to specify a vendor file via command line arguments.
 
 import argparse
 import datetime
@@ -50,30 +52,55 @@ def get_cve_data():
     return cve_json_data
 
 
-def sort_data(cve_json_data):
+def sort_cve_data(cve_json_data):
     cve_dictionary = {}
     for cve in cve_json_data['CVE_Items']:
         cve_id = cve['cve']['CVE_data_meta']['ID']
         cve_pub_date = cve['publishedDate'].split('T')[0]
         cve_last_mod = cve['lastModifiedDate'].split('T')[0]
+
+        cve_dictionary[cve_id] = {}
+        cve_dictionary[cve_id]['published'] = cve_pub_date
+        cve_dictionary[cve_id]['last_modified'] = cve_last_mod
         for cve_desc_item in cve['cve']['description']['description_data']:
             if cve_desc_item['lang'] == 'en':
                 cve_desc = cve_desc_item['value']
-
-                if "** REJECT **" not in cve_desc:
-                    print(cve_id + "\n  Published date:" + cve_pub_date + "\n  Last modified: " +
-                          cve_last_mod + "\n  Description: " + cve_desc + "\n\n")
+                cve_dictionary[cve_id]['description'] = cve_desc
 
     return cve_dictionary
+
+
+def filter_cve_by_vendor(cve_dictionary, vendor_list):
+    filtered_cves = {}
+    for vendor in vendor_list:
+        for cve in cve_dictionary:
+            if vendor in cve_dictionary[cve]['description']:
+                filtered_cves[cve] = {}
+                filtered_cves[cve]['published'] = cve_dictionary[cve]['published']
+                filtered_cves[cve]['last_modified'] = cve_dictionary[cve]['last_modified']
+                filtered_cves[cve]['description'] = cve_dictionary[cve]['description']
+
+    return filtered_cves
+
+
+def output_cves(filtered_cves):
+    print("FILTERED CVEs:\n")
+    for cve in filtered_cves:
+        print(cve + ":\nPublished: " + filtered_cves[cve]['published'] + "\nLast Modified: " +
+              filtered_cves[cve]['last_modified'] + "\nDescription: " + filtered_cves[cve]['description'] + "\n\n")
 
 
 if __name__ == '__main__':
     cve_json_data = get_cve_data()
     vendor_list = read_vendor_file()
-    cve_dictionary = sort_data(cve_json_data)
+    cve_dictionary = sort_cve_data(cve_json_data)
+    filtered_cves = filter_cve_by_vendor(cve_dictionary, vendor_list)
+    output_cves(filtered_cves)
+
 
 """
 example json cve format
 
-{'cve': {'data_type': 'CVE', 'data_format': 'MITRE', 'data_version': '4.0', 'CVE_data_meta': {'ID': 'CVE-2022-35672', 'ASSIGNER': 'psirt@adobe.com'}, 'problemtype': {'problemtype_data': [{'description': [{'lang': 'en', 'value': 'CWE-125'}]}]}, 'references': {'reference_data': [{'url': 'https://helpx.adobe.com/security/products/acrobat/apsb22-16.html', 'name': 'https://helpx.adobe.com/security/products/acrobat/apsb22-16.html', 'refsource': 'MISC', 'tags': []}]}, 'description': {'description_data': [{'lang': 'en', 'value': 'Adobe Acrobat Reader version 22.001.20085 (and earlier), 20.005.30314 (and earlier) and 17.012.30205 (and earlier) are affected by an out-of-bounds read vulnerability when parsing a crafted file, which could result in a read past the end of an allocated memory structure. An attacker could leverage this vulnerability to execute code in the context of the current user. Exploitation of this issue requires user interaction in that a victim must open a malicious file.'}]}}, 'configurations': {'CVE_data_version': '4.0', 'nodes': []}, 'impact': {'baseMetricV3': {'cvssV3': {'version': '3.1', 'vectorString': 'CVSS:3.1/AV:L/AC:L/PR:N/UI:R/S:U/C:H/I:H/A:H', 'attackVector': 'LOCAL', 'attackComplexity': 'LOW', 'privilegesRequired': 'NONE', 'userInteraction': 'REQUIRED', 'scope': 'UNCHANGED', 'confidentialityImpact': 'HIGH', 'integrityImpact': 'HIGH', 'availabilityImpact': 'HIGH', 'baseScore': 7.8, 'baseSeverity': 'HIGH'}, 'exploitabilityScore': 1.8, 'impactScore': 5.9}}, 'publishedDate': '2022-07-27T17:15Z', 'lastModifiedDate': '2022-07-27T17:15Z'}
+{'cve': {'data_type': 'CVE', 'data_format': 'MITRE', 'data_version': '4.0', 'CVE_data_meta': {'ID': 'CVE-2022-35672', 'ASSIGNER': 'psirt@adobe.com'}, 'problemtype': {'problemtype_data': [{'description': [{'lang': 'en', 'value': 'CWE-125'}]}]}, 'references': {'reference_data': [{'url': 'https://helpx.adobe.com/security/products/acrobat/apsb22-16.html', 'name': 'https://helpx.adobe.com/security/products/acrobat/apsb22-16.html', 'refsource': 'MISC', 'tags': []}]}, 'description': {'description_data': [{'lang': 'en', 'value': 'Adobe Acrobat Reader version 22.001.20085 (and earlier), 20.005.30314 (and earlier) and 17.012.30205 (and earlier) are affected by an out-of-bounds read vulnerability when parsing a crafted file, which could result in a read past the end of an allocated memory structure. An attacker could leverage this vulnerability to execute code in the context of the current user. Exploitation of this issue requires user interaction in that a victim must open a malicious file.'}]}}, 'configurations': {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           'CVE_data_version': '4.0', 'nodes': []}, 'impact': {'baseMetricV3': {'cvssV3': {'version': '3.1', 'vectorString': 'CVSS:3.1/AV:L/AC:L/PR:N/UI:R/S:U/C:H/I:H/A:H', 'attackVector': 'LOCAL', 'attackComplexity': 'LOW', 'privilegesRequired': 'NONE', 'userInteraction': 'REQUIRED', 'scope': 'UNCHANGED', 'confidentialityImpact': 'HIGH', 'integrityImpact': 'HIGH', 'availabilityImpact': 'HIGH', 'baseScore': 7.8, 'baseSeverity': 'HIGH'}, 'exploitabilityScore': 1.8, 'impactScore': 5.9}}, 'publishedDate': '2022-07-27T17:15Z', 'lastModifiedDate': '2022-07-27T17:15Z'}
 """
