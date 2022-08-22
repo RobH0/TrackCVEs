fb6bb9069c8d2ac57120da0d95519a941f544a8f
Robert <18058627+RobH0@users.noreply.github.com>
2022-08-22 14:51:53 +0100

    Removed unnecessary funcion output_cves

--- track-cves.py
+++ track-cves.py
@@ -1,12 +1,4 @@
 # track-cves.py
-
-# To implement:
-# 1. Open reports in the default browser.
-# 2. Ask user if they want to open the created reports in their default browser.
-# 3. Ask user if they want the filtered CVEs to be output to the command line.
-# 2. Refactor code.
-
-
 import argparse
 import datetime
 import io
@@ -117,45 +109,13 @@ def filter_cve_by_vendor(cve_dictionary, vendor_list):
     return filtered_cves
 
 
-def output_cves(filtered_cves):
-    print("\nFILTERED CVEs:")
-
-    print("\nHigh Severity: ")
-    for cve in filtered_cves:
-        if filtered_cves[cve].get('baseSeverity') != None and filtered_cves[cve]['baseSeverity'] == 'HIGH':
-            print("\n" + cve)
-            for key in filtered_cves[cve]:
-                print(key + ": " + str(filtered_cves[cve][key]))
-
-    print("\nMedium Severity: ")
-    for cve in filtered_cves:
-        if filtered_cves[cve].get('baseSeverity') != None and filtered_cves[cve]['baseSeverity'] == 'MEDIUM':
-            print("\n" + cve)
-            for key in filtered_cves[cve]:
-                print(key + ": " + str(filtered_cves[cve][key]))
-
-    print("\nLow Severity: ")
-    for cve in filtered_cves:
-        if filtered_cves[cve].get('baseSeverity') != None and filtered_cves[cve]['baseSeverity'] == 'LOW':
-            print("\n" + cve)
-            for key in filtered_cves[cve]:
-                print(key + ": " + str(filtered_cves[cve][key]))
-
-    print("Unspecified Severity: ")
-    for cve in filtered_cves:
-        if filtered_cves[cve].get('baseSeverity') == None:
-            print("\n" + cve)
-            for key in filtered_cves[cve]:
-                print(key + ": " + str(filtered_cves[cve][key]))
-
-
 def report_generation(filtered_cves, severity, days):
     report_details = ''
     sev_count = 0
     string_severity = str(severity)
-    report_name = string_severity.lower() + '_sev_report_'+ str(datetime.today().date()) +'.html'
+    report_name = string_severity.lower() + '_sev_report_' + \
+        str(datetime.today().date()) + '.html'
     report_file_path = ''
-
 
     if days == None:
         days = 7
@@ -165,11 +125,9 @@ def report_generation(filtered_cves, severity, days):
             sev_count += 1
             report_details += '<br><br><b><a href="https://nvd.nist.gov/vuln/detail/' + \
                 cve + '">' + cve + '</a></b>:<br> '
-            report_details += '<b>Last modified: </b>' + filtered_cves[cve]['last_modified'] + '<br>'
+            report_details += '<b>Last modified: </b>' + \
+                filtered_cves[cve]['last_modified'] + '<br>'
             report_details += str(filtered_cves[cve]['description'])
-
-
-
 
     report = '<h2>' + str(sev_count) + ' ' + string_severity + \
         ' severity CVEs relating to your vendor list over the past ' + \
@@ -184,8 +142,7 @@ def report_generation(filtered_cves, severity, days):
         report_file_path = os.path.dirname(
             os.path.realpath(report_name)) + '\\' + report_name
 
-
-        print(string_severity + " severity CVE report saved to " +  report_file_path)
+        print(string_severity + " severity CVE report saved to " + report_file_path)
     except:
         print("Error when writing to report file")
 
@@ -198,17 +155,19 @@ def generate_web_reports(filtered_cves, days):
     low_sev_file_path = report_generation(filtered_cves, 'LOW', days)
     na_sev_file_path = report_generation(filtered_cves, None, days)
 
-    report_file_path_list = [high_sev_file_path, med_sev_file_path, low_sev_file_path, na_sev_file_path]
+    report_file_path_list = [
+        high_sev_file_path, med_sev_file_path, low_sev_file_path, na_sev_file_path]
 
     return report_file_path_list
+
 
 def open_reports_in_browser(reports_list):
     correct_input = False
     print('\n')
 
-
     while correct_input == False:
-        user_response = input("Do you want the above reports to be open in your default browser (y/n)? ")
+        user_response = input(
+            "Do you want the above reports to be open in your default browser (y/n)? ")
 
         if user_response.lower() == 'y':
             print("Opening reports in your default browser")
@@ -222,14 +181,13 @@ def open_reports_in_browser(reports_list):
             print("Invalid input!")
 
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='TrackCVE')
     parser.add_argument(
-        '-f', '--file', help='Text file from which Vendor names are read.')
+        '-f', '--file', help='text file from which vendor names are read. If no file aurgument is passed, vendor names will be read from vendors.txt by default')
     parser.add_argument(
-        '-d', '--days', help='Used to only display CVEs that were released x number of days in the past.')
+        '-d', '--days', help='used to only display CVEs that were released x number of days in the past.')
     args = parser.parse_args()
 
     if args.days != None:
@@ -241,14 +199,6 @@ if __name__ == '__main__':
     vendor_list = read_vendor_file(args.file)
     cve_dictionary = sort_cve_data(cve_json_data, args.days)
     filtered_cves = filter_cve_by_vendor(cve_dictionary, vendor_list)
-    # output_cves(filtered_cves)
     report_file_path_list = generate_web_reports(filtered_cves, args.days)
     open_reports_in_browser(report_file_path_list)
     sys.exit()
-
-"""
-example json cve format
-
-{'cve': {'data_type': 'CVE', 'data_format': 'MITRE', 'data_version': '4.0', 'CVE_data_meta': {'ID': 'CVE-2022-35672', 'ASSIGNER': 'psirt@adobe.com'}, 'problemtype': {'problemtype_data': [{'description': [{'lang': 'en', 'value': 'CWE-125'}]}]}, 'references': {'reference_data': [{'url': 'https://helpx.adobe.com/security/products/acrobat/apsb22-16.html', 'name': 'https://helpx.adobe.com/security/products/acrobat/apsb22-16.html', 'refsource': 'MISC', 'tags': []}]}, 'description': {'description_data': [{'lang': 'en', 'value': 'Adobe Acrobat Reader version 22.001.20085 (and earlier), 20.005.30314 (and earlier) and 17.012.30205 (and earlier) are affected by an out-of-bounds read vulnerability when parsing a crafted file, which could result in a read past the end of an allocated memory structure. An attacker could leverage this vulnerability to execute code in the context of the current user. Exploitation of this issue requires user interaction in that a victim must open a malicious file.'}]}}, 'configurations': {
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           'CVE_data_version': '4.0', 'nodes': []}, 'impact': {'baseMetricV3': {'cvssV3': {'version': '3.1', 'vectorString': 'CVSS:3.1/AV:L/AC:L/PR:N/UI:R/S:U/C:H/I:H/A:H', 'attackVector': 'LOCAL', 'attackComplexity': 'LOW', 'privilegesRequired': 'NONE', 'userInteraction': 'REQUIRED', 'scope': 'UNCHANGED', 'confidentialityImpact': 'HIGH', 'integrityImpact': 'HIGH', 'availabilityImpact': 'HIGH', 'baseScore': 7.8, 'baseSeverity': 'HIGH'}, 'exploitabilityScore': 1.8, 'impactScore': 5.9}}, 'publishedDate': '2022-07-27T17:15Z', 'lastModifiedDate': '2022-07-27T17:15Z'}
-"""
